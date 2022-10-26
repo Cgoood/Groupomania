050e6b1a84f90407f4707a47e53a99b34304768b
Cgoood <103501243+Cgoood@users.noreply.github.com>
2022-10-26 14:33:48 +0200

    Final Version

---
+++ front/src/routes/router.js
@@ -0,0 +1,40 @@
+import WallPage from "../Pages/WallPage.vue"
+import LoginPage from "../Pages/LoginPage.vue"
+import {
+    createRouter,
+    createWebHistory
+} from "vue-router"
+
+const routes = [{
+        path: "/login",
+        component: LoginPage
+    },
+    {
+        path: "/home",
+        component: WallPage
+    },
+    {
+        path: "/",
+        redirect: "/home"
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+router.beforeEach((to, from) => {
+   
+    if (!publicPages.includes(to.path) && !isTokenLS()) {
+        return router.push("/Login")
+    }
+})
+
+const publicPages = ["/Login", "/signup"]
+
+function isTokenLS() {
+    return localStorage.getItem("token") != null
+}
+
+export default router;
